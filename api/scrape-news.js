7c1ce78e153b40007fde22f59149bb810cfda0ec
RodrigoServert <rodrigoservert@gmail.com>
2025-02-17 01:46:06 +0100

    test

--- api/scrape-news.js
+++ api/scrape-news.js
@@ -93,7 +93,7 @@ async function scrapeNews(dateStr) {
 }
 
 module.exports = async (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://rodrigoservert.github.io');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
